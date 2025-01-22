7cdbd7182b4272ac96c37eb3df1a1dae4f8fcb74
Phil Yang <phil.yang@householdcapital.com>
2025-01-22 08:27:38 +0800

    Challenge

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 // expecting time to be a string in the format like '8:15' or '12:30'
+
+const NUM_TO_ENG = {
+  1: "one",
+  3: "three",
+  8: "eight",
+  9: "nine",
+  10: "ten",
+  15: "quarter",
+  30: "half",
+}
+
 function convertTimeToWords(time) {
-  // TODO: real code goes here!
   if (time === '0:00') {
     return 'midnight';
+  } else if (time == '12:00') {
+    return 'midday';
   }
 
-  return 'half past eight';
+  // Split into hours and minutes
+  const splitParts = time.split(":");
+  const hours = Number(splitParts[0])
+  const minutes = Number(splitParts[1])
+
+  const middle = minutes > 30 ? "to" : "past";
+  const first =  minutes > 30 ? NUM_TO_ENG[60-minutes] : NUM_TO_ENG[minutes];
+  const end =  minutes > 30 ? NUM_TO_ENG[(hours % 12) + 1] : NUM_TO_ENG[hours];
+
+  if (minutes == 0) {
+    return `${NUM_TO_ENG[hours]} o'clock`;
+  }
+  return `${first} ${middle} ${end}`;
 }
+
+console.log(convertTimeToWords("09:50"));
 
 module.exports = { convertTimeToWords };
--- index.test.js
+++ index.test.js
@@ -15,4 +15,19 @@ describe('Time to words', () => {
     const timeInWords = convertTimeToWords('2:45');
     expect(timeInWords).toBe('quarter to three');
   });
+
+  it('Handles 8:00', () => {
+    const timeInWords = convertTimeToWords('8:00');
+    expect(timeInWords).toBe("eight o'clock");
+  });
+
+  it('Handles 2:50', () => {
+    const timeInWords = convertTimeToWords('2:50');
+    expect(timeInWords).toBe('ten to three');
+  });
+
+  it('Handles 12:45', () => {
+    const timeInWords = convertTimeToWords('12:45');
+    expect(timeInWords).toBe('quarter to one');
+  });
 });
